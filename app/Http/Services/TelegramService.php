8d3fdd4d156e24a5ba0a78198da5c097e3df0c1d
mearforreal <meirerbol@gmail.com>
2022-05-04 18:20:39 +0600

    create profile almost ready

--- app/Http/Services/TelegramService.php
+++ app/Http/Services/TelegramService.php
@@ -6,93 +6,97 @@ use App\Models\Country;
 use App\Models\Profile;
 use App\Models\TelegramChat;
 use App\Models\TelegramContact;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
 use Telegram\Bot\Keyboard\Keyboard;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 
-class TelegramService{
+class TelegramService
+{
 
-    public function webHookMessage($data){
+    public function webHookMessage($data)
+    {
         if (!isset($data['message']['entities']) && isset($data['message']['entities']['type']) != "bot_command") {
+
+            $profile_data = [];
             $countries = Country::get();
             $countries_btn = array();
-            foreach ($countries as $country){
-                array_push($countries_btn,[['text' => $country->title,'callback_data' => $country->id]]);
+            foreach ($countries as $country) {
+                array_push($countries_btn, [['text' => $country->title, 'callback_data' => $country->id]]);
             }
 
             if (isset($data['message']['contact'])) {
-//                $profile = new Profile();
-//                $profile->phone_number = $data['message']['contact']['phone_number'];
-//
+
+                $profile_data['phone_number'] = $data['message']['contact']['phone_number'];
 
                 $keyboard = Keyboard::make([
                     'inline_keyboard' => $countries_btn,
                     'one_time_keyboard' => true
                 ]);
-
-                //✅
-
                 Telegram::sendMessage([
                     'chat_id' => $data['message']['chat']['id'],
-                    'text' => 'Укажите номер qwe',
+                    'text' => 'Выбор города из списка',
                     'reply_markup' => $keyboard
                 ]);
-//
-//
-//                $profile->phone_number = $data['message']['contact']['phone_number'];
-//                TelegramContact::create([
-//                    'update_id' => $data['update_id'],
-//                    'message_id' => $data['message']['message_id'],
-//                    'chat_id' => $data['message']['chat']['id'],
-//                    'phone_number' => $data['message']['contact']['phone_number'],
-//                    'replied' => false,
-//                    'date' => Carbon::parse($data['message']['date']),
-//                ]);
-            } else {
-//                TelegramChat::create([
-//                    'update_id' => $data['update_id'],
-//                    'message_id' => $data['message']['message_id'],
-//                    'chat_id' => $data['message']['chat']['id'],
-//                    'message_text' => $data['message']['text'],
-//                    'replied' => false,
-//                    'date' => Carbon::parse($data['message']['date']),
-//                ]);
-                if(isset($data['callback_query'])){
-                    try {
+            } elseif (isset($data['callback_query'])) {
+                try {
+                    $countryId_selected = $data['callback_query']['data'];
+                    $country_selected = Country::where('id', '=', $countryId_selected)->first();
+                    $countries_btn = array([['text' => '✅' . $country_selected->title, 'callback_data' => $country_selected->id]]);
+                    $keyboard = Keyboard::make([
+                        'inline_keyboard' => $countries_btn,
+                        'one_time_keyboard' => true
+                    ]);
+                    Telegram::editMessageText([
+                        'chat_id' => $data['callback_query']['message']['chat']['id'],
+                        'message_id' => $data['callback_query']['message']['message_id'],
+                        'reply_markup' => $keyboard,
+                        'text' => $data['callback_query']['message']['text'],
+                    ]);
 
-                        $countryId_selected = $data['callback_query']['data'];
-                        $country_selected = Country::where('id','=',$countryId_selected)->first();
-                        $search_index = 0;
-                        for($i = 0; $i < count($countries_btn); $i++){
-                            if($country[$i][0]['callback_data'] == $countryId_selected){
-                                $search_index = $i;
-                            }
-                        }
-                        $countries_btn[$search_index] = [['text' => $country_selected->title,'' => $country_selected->id]];
+                    $profile_data['phone_number'] = $countryId_selected;
 
-                        Telegram::sendMessage([
-                            'chat_id' => $data['callback_query']['message']['chat']['id'],
-                            'text' => '✅'.json_encode($countries_btn[$search_index]['text']). ' id:'.$countries_btn['callback_data'],
-                        ]);
+                    $forceReply = Keyboard::forceReply(['force_reply' => true]);
+
+                    Telegram::sendMessage([
+                        'chat_id' => $data['callback_query']['message']['chat']['id'],
+                        'text' => 'Ввод имени и фамилии',
+                        'force_reply' => true,
+                        'reply_markup' => $forceReply
+                    ]);
 
 
-                        $btn_index = array_search($countries_btn,[['text' => $country_selected->title,'callback_data' => $country_selected->id]]);
-//                        $country_selected[$btn_index] = ['text' => '✅'.$country_selected->title,'callback_data' => $country_selected->id];
-//                        $button = Keyboard::make([
-//                            'inline_keyboard' => $countries_btn,
-//                        ]);
-//                        Log::debug([
-//                            'chat_id' => $data['callback_query']['message']['chat']['id'],
-//                            'text' =>  $data['callback_query']['message']['text'],
-//                            'reply_markup' => $button,
-//                            'message_id' => $data['callback_query']['message']['message_id'],
-//                        ]);
+                } catch (\Exception $e) {
+                    Log::debug($e);
+                }
 
-                    }catch (\Exception $e){
-                        Log::debug($e);
-                    }
-
+            }elseif (isset($data['message']['reply_to_message'])){
+                if($data['message']['reply_to_message']['text'] == "Ввод имени и фамилии"){
+                    $profile_data['full_name'] = $data['message']['text'];
+                    $forceReply = Keyboard::forceReply(['force_reply' => true,'input_field_placeholder'=>'dd/mm/YYYY']);
+                    Telegram::sendMessage([
+                        'chat_id' => $data['message']['chat']['id'],
+                        'text' => 'Ввод даты рождения',
+                        'reply_markup' => $forceReply
+                    ]);
+                }
+                if($data['message']['reply_to_message']['text'] == "Ввод даты рождения"){
+                    $profile_data['dob'] = Carbon::createFromFormat('d/m/Y',$data['message']['text']);
+                    $forceReply = Keyboard::forceReply(['force_reply' => true]);
+                    Telegram::sendMessage([
+                        'chat_id' => $data['message']['chat']['id'],
+                        'text' => 'Ввод никнейма',
+                        'reply_markup' => $forceReply
+                    ]);
+                }
+                if($data['message']['reply_to_message']['text'] == "Ввод никнейма"){
+                    $profile_data['nickname'] = $data['message']['text'];
+                    Profile::create($profile_data);
+                    Telegram::sendMessage([
+                        'chat_id' => $data['message']['chat']['id'],
+                        'text' => 'OKOK',
+                    ]);
                 }
             }
         }
