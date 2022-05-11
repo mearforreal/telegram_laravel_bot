beffbb021db754db713e5c9556109fcca0ec81c4
mearforreal <meirerbol@gmail.com>
2022-05-11 16:20:30 +0600

    remove unused files, change country to city

---
+++ database/migrations/2022_05_11_101830_change_chat_to_long_int_profiles_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeChatToLongIntProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('long_int_profiles', function (Blueprint $table) {
+            $table->bigInteger('chat_id')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('long_int_profiles', function (Blueprint $table) {
+            $table->integer('chat_id')->change();
+        });
+    }
+}
